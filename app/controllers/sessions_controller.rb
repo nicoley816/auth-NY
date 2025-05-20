87d01c994e70e05c185186885f90c5db35c037ce
Nicole Yu <nicole.yu@kellogg.northwestern.edu>
2025-05-20 16:28:34 +0000

    lecture 7

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,17 @@ class SessionsController < ApplicationController
     @user = User.find_by({"email" => params["email"]})
     # 2. if the user exists -> check if they know their password 
     if @user !=nil #if user doesn't exist, send them to 4
+
     # 3. if they know their password -> login is successful
   #if do exist, need to check password
-      if @user["password"] == params["password"] #if matches
+      if BCrypt::Password.new(@user["password"]) == params["password"] #if matches
+        
+        #add cookies here
+        #cookies["monster"] = "me like cookies"
+        #session = encrypted version of a cookie jar
+
         flash["notice"] = "Welcome"
-        redirect_to "/companies"
+        redirect_to "/companies?logged_in_user_id=#{user["id"]}"
     else
       flash["notice"] = "Nope"
       redirect_to "/login"
@@ -28,6 +34,7 @@ class SessionsController < ApplicationController
 
   def destroy
     # logout the user
+    session["user_id"] = nil
     flash["notice"] = "Goodbye."
     redirect_to "/login"
   end
--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,15 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+secret = "puppies"
+encrypted_secret = BCrypt::Password.create(secret)
+puts encrypted_secret
+
 # 2. prepare encrypted string for testing
+user_submitted_password = "dogs"
+result = BCrypt::Password.new(encrypted_secret) == user_submitted_password #the value you give it is the actual encrypted value
+puts result
+#checks if the encrypted password is the same password, should be false
+
 # 3. test secret against prepared encrypted string
+
